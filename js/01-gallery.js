f1015f090343fe87480a8f4fc5e4938928dcacc8
Roman M <98202937+Morov78@users.noreply.github.com>
2022-06-28 02:35:58 +0300

    ready

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+makeItemsGallery(galleryItems);
+galleryEl.addEventListener("click", onImageClick);
+
+function makeItemsGallery(galleryItems) {
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" target="_self" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join(" ");
+  galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+}
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const srcLargeImage = event.target.dataset.source;
+  openModal(srcLargeImage);
+}
+function openModal(image) {
+  const instance = basicLightbox.create(
+    `
+    <img src="${image}" width="800" height="600">
+`,
+    {
+      closable: false,
+      onShow: (instance) => {
+        document.onkeydown = (event) => {
+          if (event.key === "Escape") {
+            instance.close(() => (document.onkeydown = null));
+          }
+        };
+      },
+    }
+  );
+  instance.show();
+}
